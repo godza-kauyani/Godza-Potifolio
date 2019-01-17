acbd17cd0003e7fd86d1d87a3a883b58b748d22e
godza-kauyani <kauyanigodfrey@gmail.com>
2019-01-17 20:20:19 +0200

    init commit

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "css/styles.css",
+    "revision": "8562df556cb3974de610e6cbb1e36a6d"
+  },
+  {
+    "url": "index.html",
+    "revision": "8a27fb7aa23a3bfa930d95ee5dbd501c"
+  },
+  {
+    "url": "js/scripts.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
